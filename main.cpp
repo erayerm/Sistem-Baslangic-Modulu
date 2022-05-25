fbf6752c1f48c20c8cccba8cec7af2d20b208bfa
erayerm <ermemiseray@gmail.com>
2022-05-25 11:55:57 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+void islem(int);
+void program();
+void cikisKontrol(int);
+
+
+int main(){
+	
+	program();
+	
+	return 0;
+}
+
+void program(){
+	
+	cout<<"Opsiyon 1 : Arp Taraması"<< endl;
+	cout<<"Opsiyon 2 : SMB Paylaşımları"<< endl;
+	cout<<"Opsiyon 3 : SMB üzerinden İşletim Sistemi Kontrolleri"<< endl;
+	cout<<"Opsiyon 4 : SNMP Bulk DOS Saldırısı"<< endl;
+	cout<<"Seçiminizi girin (1-4):  ";
+	int secim;
+	cin>>secim;
+	islem(secim);
+	cout<<"Devam Etmek İçin 1, Programı Sonlandırmak İçin 0:  ";
+	int ikinciSecim;
+	cin>>ikinciSecim;
+	cikisKontrol(ikinciSecim);
+	
+}
+
+void cikisKontrol( int a){
+	switch(a){
+		case 0:
+			cout<<"Program Sonlandı...";
+			break;
+		case 1:
+			program();
+			break;
+		default:
+			cout<<"Hatalı Giriş Yaptınız! - Devam Etmek İçin 1, Programı Sonlandırmak İçin 0"<<endl;
+			cin>>a;
+			cikisKontrol(a);
+	}
+}
+
+void islem (int a){
+	
+	switch(a){
+		case 1:
+			//Arp Taraması
+			system("sudo arp-scan -l");
+			break;
+		case 2:
+			//SMB Paylaşımları
+			break;
+		case 3:
+			//SMB üzerinden İşletim Sistemi Kontrolleri
+			break;
+		case 4:
+			//SNMP Bulk DOS Saldırısı
+			break;
+		default:
+			cout<<"Hatalı Giriş Yaptınız! - ";
+			program();
+	}
+	
+}
+
